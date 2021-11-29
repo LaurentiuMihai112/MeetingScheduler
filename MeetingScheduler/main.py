794c615c2b8cbfec17af9e0eb7f3a0f4aa5ce041
Mihai Cristian-Laurentiu <mihai.laurentiu24@yahoo.com>
2021-11-29 20:22:31 +0200

    reworked structure

--- MeetingScheduler/main.py
+++ MeetingScheduler/main.py
@@ -1,22 +1,7 @@
-import configparser
-import psycopg2
-
 from meeting_scheduler import MeetingScheduler
 
 
-def db_connect():
-    config = configparser.RawConfigParser()
-    config.read('Database.properties')
-
-    conn = psycopg2.connect(host=config.get('DatabaseSection', 'host'),
-                            database=config.get('DatabaseSection', 'database'),
-                            user=config.get('DatabaseSection', 'user'),
-                            password=config.get('DatabaseSection', 'password'))
-    return conn
-
-
 def main():
-    print(db_connect())
     MeetingScheduler()
 
 
