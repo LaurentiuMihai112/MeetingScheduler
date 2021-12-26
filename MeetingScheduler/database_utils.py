f578acaf4fc9aa427f98bdeb09c3a7938b0b18be
Mihai Cristian-Laurentiu <mihai.laurentiu24@yahoo.com>
2021-12-26 12:40:44 +0200

    started documentation

--- MeetingScheduler/database_utils.py
+++ MeetingScheduler/database_utils.py
@@ -3,6 +3,10 @@ import psycopg2
 
 
 def db_connect():
+    """
+    Function used to connect to the database
+    :return: The connection to the database
+    """
     config = configparser.RawConfigParser()
     config.read('Database.properties')
 
@@ -18,6 +22,12 @@ class Utils:
 
     @staticmethod
     def add_person(firstname, lastname):
+        """
+        Method to add a new person to the database
+        :param firstname: Person's firstname
+        :param lastname: Person's lastname
+        :return: True on successfully adding a person to database, False otherwise
+        """
         try:
             sql = "INSERT INTO Persons (firstname, lastname) VALUES (%s, %s)"
             val = (firstname, lastname)
@@ -25,11 +35,18 @@ class Utils:
             cursor.execute(sql, val)
             Utils.connection.commit()
         except Exception as e:
-            return str(e)
+            print(str(e))
+            return False
         return True
 
     @staticmethod
     def get_person(firstname, lastname):
+        """
+        Method to check the existence of a person
+        :param firstname: Person's firstname
+        :param lastname: Person's lastname
+        :return: True if the person exists, False otherwise
+        """
         try:
             sql = "SELECT firstname, lastname FROM Persons WHERE firstname=%s and lastname=%s"
             val = (firstname, lastname)
@@ -41,10 +58,17 @@ class Utils:
                 return True
             return False
         except Exception as e:
-            return str(e)
+            print(str(e))
+            return False
 
     @staticmethod
     def get_person_id(firstname, lastname):
+        """
+        Method to get the id of a person from database
+        :param firstname: Person's firstname
+        :param lastname: Person's lastname
+        :return: id of the person for the name provided, None if no person was found
+        """
         try:
             sql = "SELECT person_id FROM Persons WHERE firstname=%s and lastname=%s"
             val = (firstname, lastname)
@@ -54,10 +78,17 @@ class Utils:
             cursor.close()
             return id
         except Exception as e:
-            return str(e)
+            print(str(e))
+            return None
 
     @staticmethod
     def add_meeting(start_date, end_date):
+        """
+        Method to schedule a new meeting
+        :param start_date: start date of the meeting
+        :param end_date: end date of the meeting
+        :return: True if the meeting was created, False otherwise
+        """
         try:
             sql = "INSERT INTO Meetings (startdate, enddate) VALUES (%s, %s)"
             val = (start_date, end_date)
@@ -66,11 +97,18 @@ class Utils:
             Utils.connection.commit()
             cursor.close()
         except Exception as e:
-            return str(e)
+            print(str(e))
+            return False
         return True
 
     @staticmethod
     def add_participants(meeting_id, person_id):
+        """
+        Method to add a participant to a meeting
+        :param meeting_id: the meeting id
+        :param person_id: the person id
+        :return: True if the participants was added, False otherwise
+        """
         try:
             sql = "INSERT INTO MeetingParticipants VALUES (%s, %s)"
             val = (meeting_id, person_id)
@@ -79,11 +117,17 @@ class Utils:
             Utils.connection.commit()
             cursor.close()
         except Exception as e:
-            return str(e)
+            print(str(e))
+            return False
         return True
 
     @staticmethod
     def get_participants(meeting_id: str):
+        """
+        Method to get all the participants to a meeting
+        :param meeting_id: the meeting id
+        :return: List of integers (participants id), None if an exception occurred
+        """
         try:
             sql = "SELECT person_id FROM MeetingParticipants WHERE meeting_id='{0}'".format(meeting_id)
             cursor = Utils.connection.cursor()
@@ -97,6 +141,12 @@ class Utils:
 
     @staticmethod
     def get_meeting_id(start_date, end_date):
+        """
+
+        :param start_date:
+        :param end_date:
+        :return:
+        """
         try:
             sql = "SELECT meeting_id FROM Meetings WHERE startdate=%s AND enddate=%s"
             val = (start_date, end_date)
@@ -110,6 +160,12 @@ class Utils:
 
     @staticmethod
     def get_all_meetings(start_date, end_date):
+        """
+
+        :param start_date:
+        :param end_date:
+        :return:
+        """
         try:
             sql = "SELECT * FROM Meetings WHERE startdate>=%s AND enddate<=%s"
             val = (start_date, end_date)
@@ -121,6 +177,10 @@ class Utils:
 
     @staticmethod
     def get_all_persons():
+        """
+
+        :return:
+        """
         try:
             sql = "SELECT CONCAT(lastname,' ',firstname) FROM Persons"
             cursor = Utils.connection.cursor()
@@ -133,6 +193,11 @@ class Utils:
 
     @staticmethod
     def get_person_by_id(id: str):
+        """
+        
+        :param id:
+        :return:
+        """
         try:
             sql = "SELECT CONCAT(lastname,' ',firstname) FROM Persons WHERE person_id=%s"
             cursor = Utils.connection.cursor()
